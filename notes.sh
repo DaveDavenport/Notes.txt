03739c23e80b3b42c8d1923813ec06846b9cf48a
QC <qball@gmpclient.nl>
2013-03-27 09:55:11 +0100

    Updates

--- notes.sh
+++ notes.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 
 NOTE_DIR=~/Notes/
+TEMP_DIR=~/Notes/.temp/
+VCS_UPDATE="git pull"
 
+
+###
+# Do not edit below this line.
+###
 source ./notes_functions.inc
 
-pushd "${NOTE_DIR}"
+# go to the Notes directory.
+pushd "${NOTE_DIR}" > /dev/null
+
+# Check temp directory.
+notes_check_directory "$TEMP_DIR"
+
+# Check updates
+notes_check_updates;
+
 
 # Get a list of NOTES files.
 NOTES_FILES=$(find -type f -iname *.txt)
 
-
+notes_info "List of notes"
 for a in ${NOTES_FILES}
 do
-get_note_name "$a" 
+notes_get_name "$a" 
 done
 
-popd # ${NOTE_DIR}
+# return.
+popd > /dev/null # ${NOTE_DIR} 
